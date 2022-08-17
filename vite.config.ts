f0da86cdb24ee88811a7d32aecbcc53dca013c68
cnotv <contact@cnotv.xyz>
2022-08-18 01:43:27 +0200

    Add support for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,30 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from "url";
+import { VitePWA } from "vite-plugin-pwa";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "logo.svg"],
+      manifest: {
+        name: "Flipbook.pics",
+        short_name: "Flipbook.pics",
+        description:
+          "Pick your video and cover to generate your own offline flipbook animation!",
+        theme_color: "#00BD7E",
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
